da97673915f8198f58f570a2b548c8b85442ab7f
Lucas TESSON <lucastesson@protonmail.com>
2024-04-25 19:24:16 +0200

    chore(sdk): merge entrypoint/model files for readability

--- sdk/entrypoint.go
+++ sdk/entrypoint.go
@@ -4,7 +4,12 @@ import (
 	"os"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
+
+// Factory define the prototype a IaC factory have to implement to be used
+// by the SDK.
+type Factory func(req *Request, resp *Response, opts ...pulumi.ResourceOption) error
 
 // Run is a SDK helper to ease the creation of challenge factories.
 // You only need to provide a factory function, and the chall-manager will
@@ -27,4 +32,32 @@ func Run(f Factory) {
 		ctx.Export("flag", resp.Flag)
 		return nil
 	})
+}
+
+// Request sent by the chall-manager SDK to the IaC factory.
+type Request struct {
+	Ctx    *pulumi.Context
+	Config *Configuration
+}
+
+// Response is created and returned by a factory to the SDK in order to
+// respond to the chall-manager API call once IaC ran.
+type Response struct {
+	ConnectionInfo pulumi.StringOutput
+	Flag           pulumi.StringOutput
+}
+
+// Configuration is the struct that contains the flattened configuration
+// from a chall-manager stack up.
+type Configuration struct {
+	Identity string
+}
+
+// Load flatten the Pulumi stack configuration into a ready-to-use struct.
+func Load(ctx *pulumi.Context, project string) *Configuration {
+	cfg := config.New(ctx, project)
+
+	return &Configuration{
+		Identity: cfg.Get("identity"),
+	}
 }
