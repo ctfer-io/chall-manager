dd774618b80d8f2cd73393937279acc6e6e62570
Lucas TESSON <lucastesson@protonmail.com>
2024-07-10 11:29:01 +0200

    docs: add examples

---
+++ examples/exposed-monopod/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/ctfer-io/chall-manager/sdk"
+	k8s "github.com/ctfer-io/chall-manager/sdk/kubernetes"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	sdk.Run(func(req *sdk.Request, resp *sdk.Response, opts ...pulumi.ResourceOption) error {
+		cm, err := k8s.NewExposedMonopod(req.Ctx, &k8s.ExposedMonopodArgs{
+			Image:      "pandatix/license-server:1",
+			Port:       8080,
+			ExposeType: k8s.ExposeIngress,
+			Hostname:   "brefctf.ctfer-io.lab",
+		}, opts...)
+		if err != nil {
+			return err
+		}
+
+		resp.ConnectionInfo = pulumi.Sprintf("curl -v https://%s", cm.URL)
+		return nil
+	})
+}
---
+++ examples/teeworlds/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+
+	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+)
+
+const internalPort = 8303
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		cfg := config.New(ctx, "teeworlds")
+		config := map[string]string{
+			"identity": cfg.Get("identity"),
+		}
+
+		labels := pulumi.ToStringMap(map[string]string{
+			"identity":  config["identity"],
+			"category":  "misc",
+			"challenge": "teeworlds",
+		})
+		if _, err := appsv1.NewDeployment(ctx, "example", &appsv1.DeploymentArgs{
+			Metadata: metav1.ObjectMetaArgs{
+				Labels: labels,
+			},
+			Spec: appsv1.DeploymentSpecArgs{
+				Selector: metav1.LabelSelectorArgs{
+					MatchLabels: labels,
+				},
+				Template: corev1.PodTemplateSpecArgs{
+					Metadata: metav1.ObjectMetaArgs{
+						Labels: labels,
+					},
+					Spec: corev1.PodSpecArgs{
+						Containers: corev1.ContainerArray{
+							corev1.ContainerArgs{
+								Name:  pulumi.String("teeworlds"),
+								Image: pulumi.String("registry.dev1.ctfer-io.lab/riftbit/teeworlds:0.7.5"),
+								Ports: corev1.ContainerPortArray{
+									corev1.ContainerPortArgs{
+										ContainerPort: pulumi.Int(internalPort),
+									},
+								},
+								Env: corev1.EnvVarArray{
+									corev1.EnvVarArgs{
+										Name:  pulumi.String("PORT"),
+										Value: pulumi.Sprintf("%d", internalPort),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}); err != nil {
+			return err
+		}
+
+		svc, err := corev1.NewService(ctx, "example", &corev1.ServiceArgs{
+			Metadata: metav1.ObjectMetaArgs{
+				Labels: labels,
+			},
+			Spec: corev1.ServiceSpecArgs{
+				Selector: labels,
+				Type:     pulumi.String("NodePort"),
+				Ports: corev1.ServicePortArray{
+					corev1.ServicePortArgs{
+						Port:       pulumi.Int(internalPort),
+						TargetPort: pulumi.Int(internalPort),
+						Protocol:   pulumi.String("UDP"),
+					},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("connection_info", svc.Spec.ApplyT(func(spec *corev1.ServiceSpec) string {
+			np := spec.Ports[0].NodePort
+			if np == nil {
+				return ""
+			}
+			return fmt.Sprintf("brefctf.ctfer-io.lab:%d", *np)
+		}).(pulumi.StringOutput))
+
+		return nil
+	})
+}
