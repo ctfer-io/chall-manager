c1f11e4e48815cbe58a8946395355d4226afafa3
Lucas Tesson <lucastesson@protonmail.com>
2026-01-09 14:10:59 +0100

    impr(swagger): fix ordering for proper info displayed

--- server/swagger.go
+++ server/swagger.go
@@ -3,6 +3,8 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"sort"
 
 	"github.com/ctfer-io/chall-manager/global"
 	"github.com/ctfer-io/chall-manager/pkg/swagger"
@@ -14,28 +16,53 @@ func addSwagger(mux *http.ServeMux) {
 		_, span := global.Tracer.Start(r.Context(), "swagger")
 		defer span.End()
 
-		swaggers := []string{
-			"challenge",
-			"instance",
-			"common", // must be last to overwrite previous attributes
-		}
 		mergedSwagger := swagger.NewMerger()
-		for _, swagger := range swaggers {
-			swaggerPath := fmt.Sprintf("./gen/api/v1/%[1]s/%[1]s.swagger.json", swagger)
+		ds, err := os.ReadDir("./gen/api/v1")
+		if err != nil {
+			http.Error(w, "Reading generated swagger directories", http.StatusInternalServerError)
+			return
+		}
+		sortDirs(ds)
+		for _, d := range ds {
+			swaggerPath := fmt.Sprintf("./gen/api/v1/%[1]s/%[1]s.swagger.json", d.Name())
 			if err := mergedSwagger.AddFile(swaggerPath); err != nil {
 				http.Error(w, "Merging swaggers", http.StatusInternalServerError)
 				return
 			}
 		}
+
 		b, err := mergedSwagger.MarshalJSON()
 		if err != nil {
 			http.Error(w, "Exporting merged swagger", http.StatusInternalServerError)
 			return
 		}
 		if _, err := w.Write(b); err != nil {
-			http.Error(w, "Writing  merged swagger", http.StatusInternalServerError)
+			http.Error(w, "Writing merged swagger", http.StatusInternalServerError)
 			return
 		}
 	})
 	mux.Handle("/swagger/", http.StripPrefix("/swagger/", http.FileServer(http.FS(swaggerui.Content))))
+}
+
+// sorts the directories in alphabetic order, and if provided, set
+// the "common" directory last (should contain the swagger global infos).
+func sortDirs(entries []os.DirEntry) {
+	sort.SliceStable(entries, func(i, j int) bool {
+		nameI := entries[i].Name()
+		nameJ := entries[j].Name()
+
+		// Check if either is the "common" directory
+		isCommonI := entries[i].IsDir() && nameI == "common"
+		isCommonJ := entries[j].IsDir() && nameJ == "common"
+
+		if isCommonI && !isCommonJ {
+			return false // i should come after j
+		}
+		if !isCommonI && isCommonJ {
+			return true // i should come before j
+		}
+
+		// Otherwise sort alphabetically
+		return nameI < nameJ
+	})
 }
